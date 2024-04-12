eee38ba2659d35a355228f94559a4b3199170327
vicky <luna280400v@gmail.com>
2024-04-11 18:36:04 -0600

    variables

---
+++ Vicky/Introduccion a la programacion 1/VARIABLES.sql
@@ -0,0 +1,18 @@
+VARIABLES
+- un solo tipo de dato
+- su valor inicial es desconocido 
+- ocupa una posición en memoria
+
+* hasta que no se registre un dato en esa variable es desconocido.
+
+REGLAS PARA NOMBRES DE VARIABLES:
+- representativo de lo que guarda/ almacena
+- debe comenzar con letras
+- no puede tener espacios en blanco
+- no puede tener espacios
+- no puede tener simbolos especiales o puntuacion
+- puede tener guion bajo
+- no puede llamrse como las palabras reservadas
+
+EJEMPLOS:
+* edad, unaEstatura, un_nombre, numero1, Peso_paciente,Apellido, apellido
